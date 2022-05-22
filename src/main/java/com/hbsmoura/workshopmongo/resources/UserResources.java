5c45b2ba9b3c150df068aa9663e2003b14c7927b
unknown <humbertobsmoura@gmail.com>
2022-05-22 17:34:59 -0300

    Endpoint for posts from user

--- src/main/java/com/hbsmoura/workshopmongo/resources/UserResources.java
+++ src/main/java/com/hbsmoura/workshopmongo/resources/UserResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.hbsmoura.workshopmongo.domain.Post;
 import com.hbsmoura.workshopmongo.domain.User;
 import com.hbsmoura.workshopmongo.dto.UserDTO;
 import com.hbsmoura.workshopmongo.services.UserService;
@@ -58,6 +59,12 @@ public class UserResources {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
