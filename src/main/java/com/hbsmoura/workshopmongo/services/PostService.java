707afaae0f38995ba97a8e542aa8eec575df19d6
unknown <humbertobsmoura@gmail.com>
2022-05-22 17:44:54 -0300

    Getting post by id with GET method

---
+++ src/main/java/com/hbsmoura/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.hbsmoura.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hbsmoura.workshopmongo.domain.Post;
+import com.hbsmoura.workshopmongo.repository.PostRepository;
+import com.hbsmoura.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
